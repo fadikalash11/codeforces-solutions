cd068cf60fc474b406b4e353afc17af53a16cbdf
MHD Fadi Kalash <fadikalash358@gmail.com>
2026-02-08 17:43:11 +0300

    Add files via upload

---
+++ CF-800-900/A. Lawn Mower.cpp
@@ -0,0 +1,124 @@
+
+
+    #include <bits/stdc++.h>
+    #define mod 1000000007
+    using namespace std;
+    using ll = long long;
+
+   // useful ==>  sort(arr, arr + n);
+
+int fun (int a )
+{
+
+if(a>0)
+fun (a/2);
+    cout<<a/2 <<endl;
+}
+
+int gcd(int x, int y) {
+    while (y != 0) {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+    return x;
+}
+
+int binaryShr(int a[],int size1 , int key)
+{
+    int low =0;
+    int high = size1-1;
+
+     while (low <=high )
+     {
+
+        int mid =(low+high)/2;
+        if (a[mid] ==key )return mid ;
+        else if (a[mid] < key )
+            low =mid+1;
+
+    else
+           high = mid -1 ;
+
+
+     }
+     return -1 ;
+
+
+
+
+}
+    ll expo(ll n, ll m)
+    {
+        ll ans = 1;
+        while (m > 0)
+        {
+            if (m & 1)
+            {
+                ans = (ans % mod * n % mod) % mod;
+            }
+            n = (n % mod * n % mod) % mod;
+            m >>= 1;
+        }
+        return ans % mod;
+    }
+
+    ll mul(ll x, ll y)
+    {
+        return (x % mod * y % mod) % mod;
+    }
+
+    bool isPrime(ll x)
+    {
+        for (int i = 2; i * i <= x; i++)
+            if (x % i == 0) return false;
+        return true;
+    }
+
+    ll  valueof(const vector<int>&a)
+    {
+        ll sum=0;
+        int mx=0;
+        for (int x : a)
+        {
+            mx=max(mx,x);
+            sum+=mx;
+
+        }
+    return sum;
+
+    }
+
+
+void solve() {
+
+int n ,w ;
+cin>>n>>w ;
+
+if (w==1)
+{
+    cout<<0<<endl;
+    return;
+}
+else
+    cout<<(w-1)*(n/w)+(n%w)<<endl;
+
+
+
+
+}
+
+    int main()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        int t;
+cin>>t;
+while(t--)
+    solve();
+
+
+        return 0;
+    }
