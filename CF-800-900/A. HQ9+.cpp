6329ab87e8e5f92ec20e0a3db455b43654538e2c
MHD Fadi Kalash <fadikalash358@gmail.com>
2026-04-20 23:41:09 +0300

    Add files via upload

---
+++ CF-800-900/A. HQ9+.cpp
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+#define mod 1000000007
+using namespace std;
+using ll = long long;
+    //  __builtin_popcount(n)    number of bits only one
+void solve() {
+string s ;
+cin>>s;
+for (int i =0;i<s.size();++i)
+{
+
+    if(s[i]=='Q'|| s[i]=='H'|| s[i]=='9')
+    {
+        cout<<"YES"<<endl;
+    return;
+
+
+    }
+}
+    cout<<"NO"<<endl;
+
+
+
+
+}
+
+    int main()
+           {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+
+            solve();
+
+
+
+
+
+
+
+
+        return 0;
+    }
+
