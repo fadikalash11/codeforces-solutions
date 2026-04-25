0fb756cd1efe62d05e71e98c6ce6efc4da403b7f
MHD Fadi Kalash <fadikalash358@gmail.com>
2026-04-26 01:18:51 +0300

    Add files via upload

---
+++ CF-800-900/A. A Wonderful Contest.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+#define mod 1000000007
+using namespace std;
+using ll = long long;
+
+
+    //  __builtin_popcount(n)    number of bits only one
+
+    void solve() {
+int n ;
+cin>>n;
+int a[n];
+for (int i =0;i<n;++i)cin>>a[i];
+for (int i =0;i<n;++i){
+
+
+    if(a[i]==100)
+    {
+        cout<<"YES"<<endl;
+    return;
+
+    }
+}
+cout<<"NO"<<endl;
+}
+
+    signed main()
+           {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+int t;
+cin>>t;
+while(t--)
+{
+            solve();
+
+
+}
+
+
+
+
+
+
+
+        return 0;
+    }
