ff249c356f4a4cad93e8038ae68040afd7b79018
MHD Fadi Kalash <fadikalash358@gmail.com>
2026-07-06 02:52:50 +0300

    Add files via upload

---
+++ CF-800-900/T. Real number.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+    using namespace std;
+    using ll = long long;
+    using lld = long double;
+    const ll Mod=1e9+7;
+    const int INF=1e9;
+
+
+    // d عدد مرات ظهور
+    ll count_divisors(ll l, ll r, ll d) {
+        return r / d - (l - 1) / d;
+    }
+    /// is n&1 number is odd  ??
+
+    ll modpow(ll base,ll n,ll mod)
+    {
+    ll re =1;
+    while(n>0)
+    {
+
+        if(n&1)re=(re*base)%mod;
+            n>>=1;
+        base=(base*base)%mod;
+    }
+    return re;
+    }
+
+    string  findsmally(ll x)
+    {
+
+        string stx =to_string(x);
+        ll  stxlen=stx.length();
+        ll p =pow(10,stxlen);
+
+        bool  checkones=true;
+        if(stx[0]!=1)
+            checkones=false;
+            else
+            {
+            for (int i =1;i<stxlen;++i)
+            {
+            if(stx[0]!=0)
+            {
+                checkones=false;
+
+                break;
+            }
+
+            }
+            if(checkones)
+                cout<<"-1"<<endl;
+
+            }
+
+        if(to_string(p)<stx)
+            return to_string(p);
+        else
+        {
+            cout<<"-1";
+        }
+
+
+
+    }
+
+    bool IsPrime(ll n )
+    {
+        if(n<=1)
+            return false;
+
+        for(int i =2;i*i<=n;++i)
+        {
+
+    if(n%i==0)
+        return false;
+        }
+    return true;
+    }
+
+
+    void solve(){
+
+
+
+double n ;
+cin>>n;
+
+ll k=ceil(n);
+cout<<k<<endl;
+if(k==n)cout<<"No";
+else cout<<"Yes";
+
+    }
+
+
+
+
+    int main(){
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+
+       solve();
+
+
+
+
+
+
+
+    return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
