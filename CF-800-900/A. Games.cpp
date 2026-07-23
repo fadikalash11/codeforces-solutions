22986e37235553bf905e95f029fd7fdc80705f0c
MHD Fadi Kalash <fadikalash358@gmail.com>
2026-07-24 00:20:21 +0300

    Add files via upload

---
+++ CF-800-900/A. Games.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+    using namespace std;
+    using ll = long long;
+    using lld = long double;
+    const ll Mod=1e9+7;
+    const int INF=1e9;
+
+
+    // d عدد مرات ظهور
+    ll count_divisors(ll l, ll r, ll d) {
+        return r / d - (l - 1) / d;
+    }
+    /// is n&1 number is odd  ??
+
+    ll modpow(ll base,ll n,ll mod)
+    {
+    ll re =1;
+    while(n>0)
+    {
+
+        if(n&1)re=(re*base)%mod;
+            n>>=1;
+        base=(base*base)%mod;
+    }
+    return re;
+    }
+
+    string  findsmally(ll x)
+    {
+
+        string stx =to_string(x);
+        ll  stxlen=stx.length();
+        ll p =pow(10,stxlen);
+
+        bool  checkones=true;
+        if(stx[0]!=1)
+            checkones=false;
+            else
+            {
+            for (int i =1;i<stxlen;++i)
+            {
+            if(stx[0]!=0)
+            {
+                checkones=false;
+
+                break;
+            }
+
+            }
+            if(checkones)
+                cout<<"-1"<<endl;
+
+            }
+
+        if(to_string(p)<stx)
+            return to_string(p);
+        else
+        {
+            cout<<"-1";
+        }
+
+
+
+    }
+
+    bool IsPrime(ll n )
+    {
+        if(n<=1)
+            return false;
+
+        for(int i =2;i*i<=n;++i)
+        {
+
+    if(n%i==0)
+        return false;
+        }
+    return true;
+    }
+
+
+    void solve(){
+
+        int n ;
+        cin>>n;
+        int a[n][2];
+        for(int i=0;i<n;++i)
+        {
+         cin>>a[i][0]>>a[i][1];
+        }
+
+        int ans =0;
+        for(int i=0;i<n;++i)
+        {
+            for(int j =0;j<n;++j)
+            {
+if(i != j && a[i][0] == a[j][1]) {
+    ans++;
+            }
+        }
+            }
+
+
+        cout<<ans<<endl;
+        }
+
+
+
+
+
+
+
+
+
+
+    int main(){
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+
+       solve();
+
+
+
+
+
+
+
+    return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
